0116af767521127369a21050a3c6f2a53bfb3f2a
Punam Kumavat <punamkumavat01@gmail.com>
2023-07-23 01:10:18 +0530

    added add and remove on checkbox fields

--- input2readme/src/components/CheckBox.js
+++ input2readme/src/components/CheckBox.js
@@ -1,20 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CheckBox = (props) => {
+    const [checked, setChecked] = useState(false);
+    const [guide, setGuide] = useState('');
+
+    const handleCheck = (e) => {
+        setChecked(!checked);
+    }
+    const handleGuide = (e) => {
+        setGuide(e.target.value);
+        console.log(guide);
+    }
+    const handleAdd = (e) => {
+        e.preventDefault();
+        const rightElement = document.querySelector(`.right.${props.variable}`);
+        const installGuide = document.createElement('div');
+        installGuide.classList.add('install-guide');
+        const input=document.createElement('input');
+        input.type='text';
+        input.className='form-checkbox-input';
+        input.id='guide';
+        input.name='guide';
+        input.onClick=handleGuide;
+        input.placeholder=props.placeholder;
+        input.value=guide;
+        input.style.display='flex';
+        installGuide.appendChild(input);
+        rightElement.appendChild(installGuide);
+        const removeBtn=document.createElement('button');
+        removeBtn.className='removebtn';
+        removeBtn.innerHTML='❌';
+        removeBtn.classList.add('removebtn');
+        installGuide.appendChild(removeBtn);
+        removeBtn.addEventListener('click',()=>{
+            installGuide.remove();
+        })
+    }
     return (
         <>
         <div className="check-group">
             <div className="left">
-                <input type="checkbox" className="form-control" id={props.variable} name={props.variable}  readOnly  />
+                <input type="checkbox" className="form-control" id={props.variable} name={props.variable}  readOnly onChange={handleCheck} />
                 <label htmlFor={props.variable} className="form-label check">{props.heading}</label>
             </div>
-            <div className="right">
+            <div className={`right ${props.variable}`}>
                 <div className="install-guide">
                     <p>{props.subhead}</p>
-                    <input type="text" className="form-checkbox-input" id="guide" name="guide " placeholder={props.placeholder} required/>
                 </div>
-            </div>                  
-        </div>
+            </div>     
+            
+            <button  className="addbtn"  onClick={handleAdd}>Add</button>       
+        </div>   
         </>
     )
 }
