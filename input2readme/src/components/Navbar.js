565b4346755a61b648a8810362474840f4a97ee3
Punam Kumavat <punamkumavat01@gmail.com>
2023-07-27 23:49:52 +0530

    Update Navbar.js

--- input2readme/src/components/Navbar.js
+++ input2readme/src/components/Navbar.js
@@ -5,15 +5,15 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineStar, AiOutlineFork, AiOutlineRead } from "react-icons/ai";
 import { FaGithub } from "react-icons/fa";
 const Navbar = () => {
-  const [starCount, setStarCount] = useState(null);
-  const [forkCount, setForkCount] = useState(null);
   const owner = "punam01"; // Replace with the repository owner's username
   const repo = "Input2Readme"; // Replace with the repository name
+  const accessToken="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";
   const navigate = useNavigate();
   const [showAlert, setShowAlert] = useState(false);
   // eslint-disable-next-line
   const [alertMessage, setAlertMessage] = useState("");
-
+  const [starCount, setStarCount] = useState(0); // eslint-disable-line no-unused-vars
+  const [forkCount, setForkCount] = useState(0); // eslint-disable-line no-unused-vars
   const handleAlertClose = () => {
     setShowAlert(false);
     setAlertMessage("");
@@ -25,17 +25,17 @@ const Navbar = () => {
         {},
         {
           headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
+            Authorization: `Bearer ${accessToken}`,
           },
         }
       );
 
       if (response.status === 202) {
-        console.log("Repository forked successfully!");
         const forksResponse = await axios.get(
           `https://api.github.com/repos/${owner}/${repo}`
         );
         setForkCount(forksResponse.data.forks_count);
+        console.log("Repository forked successfully!");
         setAlertMessage("Repository forked successfully!🎉");
       } else {
         console.error("Unable to fork the repository.");
@@ -49,37 +49,38 @@ const Navbar = () => {
     setShowAlert(true);
   };
   const handleStarClick = async () => {
-    try {
-      const response = await axios.put(
-        `https://api.github.com/user/starred/${owner}/${repo}`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
-          },
-        }
-      );
-
-      if (response.status === 204) {
-        //get current star number'
-        const starsResponse = await axios.get(
-          `https://api.github.com/repos/${owner}/${repo}`
-        );
-        setStarCount(starsResponse.data.stargazers_count);
-        setAlertMessage("Repository stared successfully!🎉");
-        // Perform any additional actions or show a success message if needed.
-      } else {
-        console.error("Unable to star the repository.");
-        // Handle the error appropriately.
+  try {
+    const response = await axios.put(
+      `https://api.github.com/user/starred/${owner}/${repo}`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
       }
-    } catch (error) {
-      console.error("Error while starring the repository:", error);
-      setAlertMessage(
-        "Error while starring the repository. Please try again later."
+    );
+
+    if (response.status === 204) {
+      const starsResponse = await axios.get(
+        `https://api.github.com/repos/${owner}/${repo}`
       );
+      setStarCount(starsResponse.data.stargazers_count);
+      setAlertMessage("Repository starred successfully!🎉");
       setShowAlert(true);
+      // Perform any additional actions or show a success message if needed.
+    } else {
+      console.error("Unable to star the repository.");
+      // Handle the error appropriately.
     }
-  };
+  } catch (error) {
+    console.error("Error while starring the repository:", error);
+    setAlertMessage(
+      "Error while starring the repository. Please try again later."
+    );
+    setShowAlert(true);
+  }
+};
+
   const handleBlogClick = () => {
     window.location.href = "https://medium.com/@punamkumavat01";
   };
@@ -123,7 +124,7 @@ const Navbar = () => {
             }}
           >
             <AiOutlineStar className="icon" />
-            <p>Star this repository {starCount !== null && { starCount }}</p>
+            <p>Star this repository {starCount?starCount:""}</p>
           </li>
           <li
             onClick={async () => {
@@ -131,7 +132,7 @@ const Navbar = () => {
             }}
           >
             <AiOutlineFork className="icon" />
-            <p>Fork this repository {forkCount !== null && { forkCount }}</p>
+            <p>Fork this repository {forkCount?forkCount:""}</p>
           </li>
         </ul>
       </nav>
